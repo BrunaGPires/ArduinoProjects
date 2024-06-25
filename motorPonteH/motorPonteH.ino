52be3c9b23ab90459cf48ff055676a6be0cc5044
Bruna Pires <99612473+BrunaGPires@users.noreply.github.com>
2024-06-25 10:45:18 -0300

    Add files via upload

--- motorPonteH/motorPonteH.ino
+++ motorPonteH/motorPonteH.ino
@@ -1,39 +1,60 @@
-int fowardPin = 8;
-int backwardPin = 12;
+#define ENB 6 //PWM
+#define IN3 7
+#define IN4 8
 
-int btnOnePin = 2;
-int btnTwoPin = 3;
-int btnOne;
-int btnTwo;
-
-void setup()
-{
-  pinMode(fowardPin, OUTPUT);
-  pinMode(backwardPin, OUTPUT);
-  pinMode(btnOnePin, INPUT_PULLUP);
-  pinMode(btnTwoPin, INPUT_PULLUP);
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  //digitalWrite(IN3, LOW);
+  //digitalWrite(IN4, LOW);
+  //analogWrite(ENB, 0);
 }
 
-void loop()
-{
-  btnOne = digitalRead(btnOnePin);
-  btnTwo = digitalRead(btnTwoPin);
+void loop() {
+  // motores para frente
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  acelera();
+  naoAcelera();
+
+  delay(500);
   
-  if(btnOne == HIGH)
-  {
-    digitalWrite(fowardPin, LOW);
+  //motores para trás
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  acelera();
+  naoAcelera();
+
+  delay(500);
+
+  //motores para trás full speed
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  analogWrite(ENB, 255);
+
+  delay(1500);
+
+  analogWrite(ENB, 0);
+
+  delay(500);
+}
+
+void acelera() {
+  for (int i = 0; i < 255; i++) {
+    analogWrite(ENB, i);
+    delay(20);
   }
-  else 
-  {
-    digitalWrite(fowardPin, HIGH);
-  }
-  
-  if(btnTwo == HIGH)
-  {
-    digitalWrite(backwardPin, LOW);
-  }
-  else
-  {
-    digitalWrite(backwardPin, HIGH);
+}
+
+void naoAcelera() {
+  for (int i = 255; i > 0; --i) {
+    analogWrite(ENB, i);
+    delay(20);
   }
 }
