19ce585e693c86e1571e00f1e0e4bd15ab7bc1e2
Bruna Pires <bruna.gpires@hotmail.com>
2024-07-05 12:17:49 -0300

    Create morse code

---
+++ codigoMorse/codigoMorse.ino
@@ -0,0 +1,227 @@
+//componentes
+#define LED 2
+#define BUZZER 4
+
+//armazena palavra
+String str = "";
+//verifica se já foi
+bool status = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
+  Serial.setTimeout(10);
+  Serial.println("Starting...");
+  delay(1000);
+}
+
+void loop() {
+  receive();
+
+  if (status == false) {
+    translate();
+
+    // limpa a string depois de traduzir e reseta o status
+    str = "";
+    status = false;    
+  }
+  
+}
+
+//lê o que usuario digitou no serial
+void receive() {
+  if (Serial.available() > 0) {
+    str = Serial.readStringUntil('\n');
+    str.trim();
+    Serial.println(str);
+  }
+}
+
+//traduz o caractere
+void translate() {
+  for (int i = 0; i < str.length(); i++) {
+    char charact = toLowerCase(str.charAt(i));
+
+    switch(charact) {
+      case 'a':
+        dot();
+        dash();
+        break;
+      case 'b':
+        dash();
+        dot();
+        dot();
+        dot();
+        break;
+      case 'c':
+        dash();
+        dot();
+        dash();
+        dot();
+        break;
+      case 'd':
+        dash();
+        dot();
+        dot();
+        break;
+      case 'e':
+        dot();
+        break;
+      case 'f':
+        dot();
+        dot();
+        dash();
+        dot();
+        break;
+      case 'g':
+        dash();
+        dash();
+        dot();
+        break;
+      case 'h':
+        dot();
+        dot();
+        dot();
+        dot();
+        break;
+      case 'i':
+        dot();
+        dot();
+        break;
+      case 'j':
+        dot();
+        dash();
+        dash();
+        dash();
+        break;
+      case 'k':
+        dash();
+        dot();
+        dash();
+        break;
+      case 'l':
+        dot();
+        dash();
+        dot();
+        dot();
+        break;
+      case 'm':
+        dash();
+        dash();
+        break;
+      case 'n':
+        dash();
+        dot();
+        break;
+      case 'o':
+        dash();
+        dash();
+        dash();
+        break;
+      case 'p':
+        dot();
+        dash();
+        dash();
+        dot();
+        break;
+      case 'q':
+        dash();
+        dash();
+        dot();
+        dash();
+        break;
+      case 'r':
+        dash();
+        dot();
+        dash();
+        break;
+      case 's':
+        dot();
+        dot();
+        dot();
+        break;
+      case 't':
+        dash();
+        break;
+      case 'u':
+        dot();
+        dot();
+        dash();
+        break;
+      case 'v':
+        dot();
+        dot();
+        dot();
+        dash();
+        break;
+      case 'w':
+        dot();
+        dash();
+        dash();
+        break;
+      case 'x':
+        dash();
+        dot();
+        dot();
+        dash();
+        break;
+      case 'y':
+        dash();
+        dot();
+        dash();
+        dash();
+        break;
+      case 'z':
+        dash();
+        dash();
+        dot();
+        dot();
+        break;
+      default:
+      Serial.println("Invalid charactere!");
+      break;
+    }
+  
+    if (i <= str.length() - 1) {
+      delay(600); //delay entre caracteres
+    }
+
+    status = true;
+  }
+}
+
+
+//pisca o led
+void dot() {
+  buzzerDot();
+  digitalWrite(LED, HIGH);
+  delay(200);
+  Serial.println(".");
+  digitalWrite(LED, LOW);
+  delay(200);
+}
+
+void dash() {
+  buzzerDash();
+  digitalWrite(LED, HIGH);
+  delay(600);
+  Serial.println("-");
+  digitalWrite(LED, LOW);
+  delay(200); 
+}
+
+//buzer bipbip
+void buzzerDot() {
+  tone(BUZZER, 440);
+  delay(150);
+  noTone(BUZZER);
+}
+
+void buzzerDash() {
+  tone(BUZZER, 400);
+  delay(600);
+  noTone(BUZZER);
+}
